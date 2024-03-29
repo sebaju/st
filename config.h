0754d34736134a58f572aa48029a65860787dcd8
sebajune <sal@aeo.li>
2024-03-28 23:40:34 -0400

    Change scroll speed

--- config.h
+++ config.h
@@ -210,7 +210,7 @@ ResourcePref resources[] = {
  * Internal mouse shortcuts.
  * Beware that overloading Button1 will disable the selection.
  */
-const unsigned int mousescrollincrement = 1;
+const unsigned int mousescrollincrement = 3;
 static MouseShortcut mshortcuts[] = {
 	/* mask                 button   function        argument       release */
         { XK_ANY_MOD,           Button4, kscrollup,      {.i = 1},		0, /* !alt */ -1 },
